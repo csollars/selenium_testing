b59c85d32c97c62648dcae77d54cc9601c195534
Christopher Sollars <as5540@wayne.edu>
2019-02-13 17:07:09 -0500

    working on ccq tests

--- selenium/src/ccq/CompanyPage.java
+++ selenium/src/ccq/CompanyPage.java
@@ -3,6 +3,6 @@ package ccq;
 import org.openqa.selenium.By;
 
 public class CompanyPage {
-	public static final By pageLocator = By.xpath("");
+	public static final By pageLocator = By.xpath("//*[@id='candidate-user-management-left-header-content']");
 	public static final String pageDescription = "Company Page";
 }
